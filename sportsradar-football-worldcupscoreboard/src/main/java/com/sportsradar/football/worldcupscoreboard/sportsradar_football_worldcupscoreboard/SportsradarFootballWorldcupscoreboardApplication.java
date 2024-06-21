4f7213f3feb96f222c8875370271aa26b735bdf2
clkgud <Lavan.KumarGudipudi@mastercardpaymentservices.com>
2024-06-21 15:15:05 +0200

    Intial commit

---
+++ sportsradar-football-worldcupscoreboard/src/main/java/com/sportsradar/football/worldcupscoreboard/sportsradar_football_worldcupscoreboard/SportsradarFootballWorldcupscoreboardApplication.java
@@ -0,0 +1,13 @@
+package com.sportsradar.football.worldcupscoreboard.sportsradar_football_worldcupscoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsradarFootballWorldcupscoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsradarFootballWorldcupscoreboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportsradar/football/worldcupscoreboard/sportsradar_football_worldcupscoreboard/SportsradarFootballWorldcupscoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsradar.football.worldcupscoreboard.sportsradar_football_worldcupscoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsradarFootballWorldcupscoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
